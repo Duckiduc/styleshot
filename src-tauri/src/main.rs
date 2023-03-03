c02dbce1008b05ae1c4910fb4c91ab207f7accf3
Duckiduc <git@ducki.dev>
2023-03-03 13:25:05 +0100

    Add drag handler to tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,41 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+use serde::Deserialize;
+use tauri::api::drag_and_drop::{Files, Item};
+use tauri::AppBuilder;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+#[derive(Deserialize)]
+struct DraggedFile {
+    path: String,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_drag_and_drop::DragAndDrop::default())
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("failed to run app");
+    let app = AppBuilder::new()
+        .setup(|_webview, _source| {
+            tauri::api::drag_and_drop::on(
+                |event| {
+                    // The file(s) have been dropped on the app
+                    let files: Vec<DraggedFile> = event
+                        .items()
+                        .iter()
+                        .map(|item| {
+                            let file = item.file().unwrap();
+                            DraggedFile {
+                                path: file.path().unwrap().to_str().unwrap().to_owned(),
+                            }
+                        })
+                        .collect();
+                    println!("Files dropped: {:?}", files);
+                },
+                |_error| {
+                    // There was an error handling the drag and drop event
+                    println!("Error handling drag and drop event!");
+                },
+            );
+        })
+        .build();
+
+    app.run();
 }
